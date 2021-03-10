51c6b8bc0a7d4897f9f4baf9e4e559c81011eca6
AndreySunstrider <plaksinandrey94@gmail.com>
2021-03-10 17:05:17 +0300

    feature: added rest module(inside platform module)
    [CH-012]

---
+++ platform/rest-clubhouse/build.gradle.kts
@@ -0,0 +1,25 @@
+
+plugins {
+    id(BuildPlugins.Ids.kotlinModule)
+    id(BuildPlugins.Ids.kotlinxSerialization)
+}
+
+dependencies {
+    implementation(project(":platform:logger"))
+    implementation(project(":platform:domain"))
+    implementation(project(":platform:networking"))
+
+    implementation(Libraries.kotlinStdlib)
+    implementation(Libraries.okhttp)
+    implementation(Libraries.okhttpLogger)
+    implementation(Libraries.retrofit)
+    implementation(Libraries.kotlinSerializationCore)
+    implementation(Libraries.kotlinSerializationJson)
+    implementation(Libraries.coroutinesCore)
+    implementation(Libraries.kodein)
+
+    testImplementation(Libraries.jUnit)
+    testImplementation(Libraries.assertj)
+    testImplementation(Libraries.burster)
+    testImplementation(Libraries.mockWebServer)
+}
