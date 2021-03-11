ac46a035b4802f7c58779b5749f30fe01d5ad739
AndreySunstrider <plaksinandrey94@gmail.com>
2021-03-11 11:11:16 +0300

    feature: added dependencies, assets, welcome activity layout
    * created activity_welcome.xml
    * added assets
    
    [CH-015]

--- features/auth/build.gradle.kts
+++ features/auth/build.gradle.kts
@@ -4,5 +4,25 @@ plugins {
 }
 
 dependencies {
+    implementation(project(":platform:logger"))
+    implementation(project(":platform:domain"))
+    implementation(project(":platform:shared-assets"))
+    implementation(project(":platform:shared-utilities"))
+    implementation(project(":platform:navigator"))
 
+    implementation(Libraries.kotlinStdlib)
+    implementation(Libraries.kodein)
+    implementation(Libraries.coroutinesCore)
+    implementation(Libraries.coroutinesAndroid)
+    implementation(Libraries.lifecycleExtensions)
+    implementation(Libraries.lifecycleRuntime)
+    implementation(Libraries.lifecycleViewModel)
+    implementation(Libraries.coreAndroidx)
+    implementation(Libraries.appCompat)
+    implementation(Libraries.materialDesign)
+
+    testImplementation(project(":platform:coroutines-testutils"))
+    testImplementation(Libraries.jUnit)
+    testImplementation(Libraries.assertj)
+    testImplementation(Libraries.turbine)
 }
